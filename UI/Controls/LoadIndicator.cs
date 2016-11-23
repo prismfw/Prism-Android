0b41d71bce9c58237860fa7c3f39007ef06a44d7
jonathan-git <jonathan-git@users.noreply.github.com>
2016-11-22 22:40:57 -0600

    Implemented new LoadIndicator APIs.

---
+++ UI/Controls/LoadIndicator.cs
@@ -0,0 +1,356 @@
+/*
+Copyright (C) 2016  Prism Framework Team
+
+This file is part of the Prism Framework.
+
+The Prism Framework is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+The Prism Framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+using System;
+using Android.App;
+using Android.Graphics.Drawables;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Prism.Native;
+using Prism.Systems;
+using Prism.UI;
+using Prism.UI.Media;
+
+namespace Prism.Android.UI.Controls
+{
+    /// <summary>
+    /// Represents an Android implementation for an <see cref="INativeLoadIndicator"/>.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [Register(typeof(INativeLoadIndicator))]
+    public class LoadIndicator : FrameLayout, INativeLoadIndicator
+    {
+        /// <summary>
+        /// Occurs when this instance has been attached to the visual tree and is ready to be rendered.
+        /// </summary>
+        public event EventHandler Loaded;
+        
+        /// <summary>
+        /// Occurs when the value of a property is changed.
+        /// </summary>
+        public event EventHandler<FrameworkPropertyChangedEventArgs> PropertyChanged;
+        
+        /// <summary>
+        /// Occurs when this instance has been detached from the visual tree.
+        /// </summary>
+        public event EventHandler Unloaded;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether animations are enabled for this instance.
+        /// </summary>
+        public bool AreAnimationsEnabled
+        {
+            get { return areAnimationsEnabled; }
+            set
+            {
+                if (value != areAnimationsEnabled)
+                {
+                    areAnimationsEnabled = value;
+                    OnPropertyChanged(Visual.AreAnimationsEnabledProperty);
+                }
+            }
+        }
+        private bool areAnimationsEnabled;
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests an arrangement of its children.
+        /// </summary>
+        public ArrangeRequestHandler ArrangeRequest { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the background of the indicator.
+        /// </summary>
+        public new Brush Background
+        {
+            get { return background; }
+            set
+            {
+                if (value != background)
+                {
+                    (background as ImageBrush).ClearImageHandler(OnBackgroundImageLoaded);
+
+                    background = value;
+                    base.Background = (background.GetDrawable(OnBackgroundImageLoaded) ??
+                        (Drawable)Prism.Application.Current.Resources[global::Android.Resource.Attribute.Background]);
+                    
+                    OnPropertyChanged(Prism.UI.Controls.LoadIndicator.BackgroundProperty);
+                }
+            }
+        }
+        private Brush background;
+        
+        /// <summary>
+        /// Gets or sets the content for the indicator.
+        /// </summary>
+        public object Content
+        {
+            get { return GetChildAt(0); }
+            set
+            {
+                if (ChildCount > 0)
+                {
+                    RemoveViewAt(0);
+                }
+
+                var view = value as global::Android.Views.View;
+                if (view != null)
+                {
+                    AddView(view, 0);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets a <see cref="Rectangle"/> that represents the size and position of the element relative to its parent container.
+        /// </summary>
+        public Rectangle Frame
+        {
+            get
+            {
+                return new Rectangle(Left / Device.Current.DisplayScale, Top / Device.Current.DisplayScale,
+                    Width / Device.Current.DisplayScale, Height / Device.Current.DisplayScale);
+            }
+            set
+            {
+                // Frame won't actually size the window or its decor view.
+                // We might change this in the future, but for now it's intentional.
+                // Apps that need to change the size can set the WindowSize property through SetValue.
+                Left = (int)(value.Left * Device.Current.DisplayScale);
+                Top = (int)(value.Top * Device.Current.DisplayScale);
+                Right = (int)(value.Right * Device.Current.DisplayScale);
+                Bottom = (int)(value.Bottom * Device.Current.DisplayScale);
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can be considered a valid result for hit testing.
+        /// </summary>
+        public bool IsHitTestVisible
+        {
+            get { return (dialog.Window.Attributes.Flags & WindowManagerFlags.NotTouchable) == 0; }
+            set
+            {
+                if (value != IsHitTestVisible)
+                {
+                    if (value)
+                    {
+                        dialog.Window.ClearFlags(WindowManagerFlags.NotTouchable);
+                    }
+                    else
+                    {
+                        dialog.Window.SetFlags(WindowManagerFlags.NotTouchable, WindowManagerFlags.NotTouchable);
+                    }
+                    
+                    OnPropertyChanged(Visual.IsHitTestVisibleProperty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been loaded and is ready for rendering.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests a measurement of itself and its children.
+        /// </summary>
+        public MeasureRequestHandler MeasureRequest { get; set; }
+        
+        /// <summary>
+        /// Gets or sets transformation information that affects the rendering position of this instance.
+        /// </summary>
+        public INativeTransform RenderTransform
+        {
+            get { return renderTransform; }
+            set
+            {
+                if (value != renderTransform)
+                {
+                    (renderTransform as Media.Transform)?.RemoveView(this);
+                    renderTransform = value;
+                    
+                    var transform = renderTransform as Media.Transform;
+                    if (transform == null)
+                    {
+                        Animation = renderTransform as global::Android.Views.Animations.Animation;
+                    }
+                    else
+                    {
+                        transform.AddView(this);
+                    }
+
+                    OnPropertyChanged(Visual.RenderTransformProperty);
+                }
+            }
+        }
+        private INativeTransform renderTransform;
+
+        /// <summary>
+        /// Gets or sets the visual theme that should be used by this instance.
+        /// </summary>
+        public Theme RequestedTheme { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the size of the window in which the indicator is presented.
+        /// </summary>
+        public Size WindowSize
+        {
+            get
+            {
+                return new Size(dialog.Window.DecorView.Width / Device.Current.DisplayScale,
+                    dialog.Window.DecorView.Height / Device.Current.DisplayScale);
+            }
+            set
+            {
+                dialog.Window.SetLayout((int)value.Width, (int)value.Height);
+            }
+        }
+
+        private readonly Dialog dialog;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadIndicator"/> class.
+        /// </summary>
+        public LoadIndicator()
+            : base(Application.MainActivity)
+        {
+            dialog = new ProgressDialog(Context);
+        }
+
+        /// <summary>
+        /// Removes the indicator from view.
+        /// </summary>
+        public void Hide()
+        {
+            dialog.Dismiss();
+        }
+        
+        /// <summary>
+        /// Invalidates the arrangement of this instance's children.
+        /// </summary>
+        public void InvalidateArrange()
+        {
+            RequestLayout();
+        }
+
+        /// <summary>
+        /// Invalidates the measurement of this instance and its children.
+        /// </summary>
+        public void InvalidateMeasure()
+        {
+            RequestLayout();
+        }
+
+        /// <summary>
+        /// Measures the element and returns its desired size.
+        /// </summary>
+        /// <param name="constraints">The width and height that the element is not allowed to exceed.</param>
+        public Size Measure(Size constraints)
+        {
+            var parentView = Parent as global::Android.Views.View ?? dialog.Window.DecorView;
+            return new Size(parentView.Width / Device.Current.DisplayScale, parentView.Height / Device.Current.DisplayScale);
+        }
+
+        /// <summary>
+        /// Displays the indicator.
+        /// </summary>
+        public void Show()
+        {
+            dialog.Show();
+            dialog.SetCancelable(false);
+            dialog.SetContentView(this);
+        }
+        
+        /// <summary>
+        /// This is called when the view is attached to a window.
+        /// </summary>
+        protected override void OnAttachedToWindow()
+        {
+            base.OnAttachedToWindow();
+            OnLoaded();
+        }
+
+        /// <summary>
+        /// This is called when the view is detached from a window.
+        /// </summary>
+        protected override void OnDetachedFromWindow()
+        {
+            base.OnDetachedFromWindow();
+            OnUnloaded();
+        }
+        
+        /// <summary>
+        /// Called from layout when this view should assign a size and position to each of its children.
+        /// </summary>
+        /// <param name="changed"></param>
+        /// <param name="left">Left position, relative to parent.</param>
+        /// <param name="top">Top position, relative to parent.</param>
+        /// <param name="right">Right position, relative to parent.</param>
+        /// <param name="bottom">Bottom position, relative to parent.</param>
+        protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+        {
+            MeasureRequest(false, null);
+            ArrangeRequest(false, null);
+
+            for (int i = 0; i < ChildCount; i++)
+            {
+                var child = GetChildAt(i);
+                child.Layout(child.Left, child.Top, child.Right, child.Bottom);
+            }
+        }
+
+        /// <summary>
+        /// Called when a property value is changed.
+        /// </summary>
+        /// <param name="pd">A property descriptor describing the property whose value has been changed.</param>
+        protected virtual void OnPropertyChanged(PropertyDescriptor pd)
+        {
+            PropertyChanged(this, new FrameworkPropertyChangedEventArgs(pd));
+        }
+
+        private void OnBackgroundImageLoaded(object sender, EventArgs e)
+        {
+            base.Background = (background.GetDrawable(null) ??
+                (Drawable)Prism.Application.Current.Resources[global::Android.Resource.Attribute.Background]);
+        }
+        
+        private void OnLoaded()
+        {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                OnPropertyChanged(Visual.IsLoadedProperty);
+                Loaded(this, EventArgs.Empty);
+            }
+        }
+
+        private void OnUnloaded()
+        {
+            if (IsLoaded)
+            {
+                IsLoaded = false;
+                OnPropertyChanged(Visual.IsLoadedProperty);
+                Unloaded(this, EventArgs.Empty);
+            }
+        }
+    }
+}
