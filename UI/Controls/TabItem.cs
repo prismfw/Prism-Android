4ac839e1872cebf1ce78c1d0ab6574dde367cf62
jonathan-git <jonathan-git@users.noreply.github.com>
2016-12-06 22:32:19 -0600

    Implemented new TabItem APIs.

--- UI/Controls/TabItem.cs
+++ UI/Controls/TabItem.cs
@@ -20,17 +20,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 using System;
-using System.Linq;
+using Android.App;
 using Android.Graphics;
 using Android.Runtime;
-using Android.Support.Design.Widget;
 using Android.Text;
 using Android.Util;
+using Android.Views;
 using Android.Widget;
 using Prism.Native;
 using Prism.Systems;
+using Prism.UI;
 using Prism.UI.Media;
-using Android.App;
 
 namespace Prism.Android.UI.Controls
 {
@@ -42,9 +42,41 @@ namespace Prism.Android.UI.Controls
     public class TabItem : LinearLayout, INativeTabItem
     {
         /// <summary>
+        /// Occurs when this instance has been attached to the visual tree and is ready to be rendered.
+        /// </summary>
+        public event EventHandler Loaded;
+
+        /// <summary>
         /// Occurs when the value of a property is changed.
         /// </summary>
         public event EventHandler<FrameworkPropertyChangedEventArgs> PropertyChanged;
+        
+        /// <summary>
+        /// Occurs when this instance has been detached from the visual tree.
+        /// </summary>
+        public event EventHandler Unloaded;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether animations are enabled for this instance.
+        /// </summary>
+        public bool AreAnimationsEnabled
+        {
+            get { return areAnimationsEnabled; }
+            set
+            {
+                if (value != areAnimationsEnabled)
+                {
+                    areAnimationsEnabled = value;
+                    OnPropertyChanged(Visual.AreAnimationsEnabledProperty);
+                }
+            }
+        }
+        private bool areAnimationsEnabled;
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests an arrangement of its children.
+        /// </summary>
+        public ArrangeRequestHandler ArrangeRequest { get; set; }
 
         /// <summary>
         /// Gets or sets the object that acts as the content of the item.
@@ -169,6 +201,11 @@ namespace Prism.Android.UI.Controls
         private Brush foreground;
 
         /// <summary>
+        /// Gets or sets a <see cref="Rectangle"/> that represents the size and position of the element relative to its parent container.
+        /// </summary>
+        public Rectangle Frame { get; set; }
+
+        /// <summary>
         /// Gets or sets an <see cref="INativeImageSource"/> for an image to display with the item.
         /// </summary>
         public INativeImageSource ImageSource
@@ -194,6 +231,67 @@ namespace Prism.Android.UI.Controls
             }
         }
         private INativeImageSource imageSource;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can be considered a valid result for hit testing.
+        /// </summary>
+        public bool IsHitTestVisible
+        {
+            get { return isHitTestVisible; }
+            set
+            {
+                if (value != isHitTestVisible)
+                {
+                    isHitTestVisible = value;
+                    OnPropertyChanged(Visual.IsHitTestVisibleProperty);
+                }
+            }
+        }
+        private bool isHitTestVisible = true;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been loaded and is ready for rendering.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests a measurement of itself and its children.
+        /// </summary>
+        public MeasureRequestHandler MeasureRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets transformation information that affects the rendering position of this instance.
+        /// </summary>
+        public INativeTransform RenderTransform
+        {
+            get { return renderTransform; }
+            set
+            {
+                if (value != renderTransform)
+                {
+                    (renderTransform as Media.Transform)?.RemoveView(this);
+                    renderTransform = value;
+
+                    var transform = renderTransform as Media.Transform;
+                    if (transform == null)
+                    {
+                        Animation = renderTransform as global::Android.Views.Animations.Animation;
+                    }
+                    else
+                    {
+                        transform.AddView(this);
+                    }
+                    
+                    OnPropertyChanged(Visual.RenderTransformProperty);
+                }
+            }
+        }
+        private INativeTransform renderTransform;
+
+        /// <summary>
+        /// Gets or sets the visual theme that should be used by this instance.
+        /// </summary>
+        public Theme RequestedTheme { get; set; }
 
         /// <summary>
         /// Gets or sets the title for the item.
@@ -236,10 +334,83 @@ namespace Prism.Android.UI.Controls
             };
             TextView.SetMaxLines(2);
 
-            Orientation = Orientation.Vertical;
+            Orientation = global::Android.Widget.Orientation.Vertical;
             AddView(ImageView, new LinearLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent));
             AddView(TextView, new LinearLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent));
             SetMinimumWidth(200);
+        }
+
+        /// <summary>
+        /// Invalidates the arrangement of this instance's children.
+        /// </summary>
+        public void InvalidateArrange()
+        {
+            RequestLayout();
+        }
+
+        /// <summary>
+        /// Invalidates the measurement of this instance and its children.
+        /// </summary>
+        public void InvalidateMeasure()
+        {
+            RequestLayout();
+        }
+
+        /// <summary>
+        /// Measures the element and returns its desired size.
+        /// </summary>
+        /// <param name="constraints">The width and height that the element is not allowed to exceed.</param>
+        public Size Measure(Size constraints)
+        {
+            base.OnMeasure(MeasureSpec.MakeMeasureSpec(1, MeasureSpecMode.Unspecified),
+                MeasureSpec.MakeMeasureSpec(1, MeasureSpecMode.Unspecified));
+
+            return new Size(MeasuredWidth, MeasuredHeight) / Device.Current.DisplayScale;
+        }
+
+        /// <summary></summary>
+        /// <param name="e"></param>
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            if (!isHitTestVisible)
+            {
+                return true;
+            }
+            
+            return base.OnTouchEvent(e);
+        }
+
+        /// <summary>
+        /// This is called when the view is attached to a window.
+        /// </summary>
+        protected override void OnAttachedToWindow()
+        {
+            base.OnAttachedToWindow();
+            OnLoaded();
+        }
+
+        /// <summary>
+        /// This is called when the view is detached from a window.
+        /// </summary>
+        protected override void OnDetachedFromWindow()
+        {
+            base.OnDetachedFromWindow();
+            OnUnloaded();
+        }
+
+        /// <summary>
+        /// Called from layout when this view should assign a size and position to each of its children.
+        /// </summary>
+        /// <param name="changed"></param>
+        /// <param name="left">Left position, relative to parent.</param>
+        /// <param name="top">Top position, relative to parent.</param>
+        /// <param name="right">Right position, relative to parent.</param>
+        /// <param name="bottom">Bottom position, relative to parent.</param>
+        protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+        {
+            MeasureRequest(false, null);
+            ArrangeRequest(false, null);
+            base.OnLayout(changed, Left, Top, Right, Bottom);
         }
 
         /// <summary>
@@ -261,6 +432,26 @@ namespace Prism.Android.UI.Controls
             ImageView.SetImageBitmap(imageSource.GetImageSource());
             imageSource.GetImageSource().PrepareToDraw();
             ImageView.Invalidate();
+        }
+
+        private void OnLoaded()
+        {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                OnPropertyChanged(Visual.IsLoadedProperty);
+                Loaded(this, EventArgs.Empty);
+            }
+        }
+
+        private void OnUnloaded()
+        {
+            if (IsLoaded)
+            {
+                IsLoaded = false;
+                OnPropertyChanged(Visual.IsLoadedProperty);
+                Unloaded(this, EventArgs.Empty);
+            }
         }
     }
 }
